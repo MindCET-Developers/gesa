7dbe6a6510d3a15341a76d5c18f896b2db561ec9
ilanmichalby <info@mindcet.io>
2026-07-15 18:02:55 +0300

    Remove background from logo circles
    Make background transparent to prepare for dark logo versions.

--- components/semifinals/PartnerLogo.tsx
+++ components/semifinals/PartnerLogo.tsx
@@ -41,7 +41,7 @@ export function PartnerLogo({ partner, logo }: { partner: string; logo?: string 
         alt={`${partner} logo`}
         width={48}
         height={48}
-        className="h-12 w-12 shrink-0 rounded-full border border-gray-300 bg-gray-200 object-contain"
+        className="h-12 w-12 shrink-0 rounded-full border border-gray-300 object-contain"
       />
     );
   }
