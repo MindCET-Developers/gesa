7b2a09a3a3d30e81072ca31920d7aaa0ec73e83d
ilanmichalby <info@mindcet.io>
2026-07-15 17:45:23 +0300

    Improve semifinals partner logo visibility
    Change filter from saturate-0 brightness-50 to invert brightness-125
    for better visibility of both dark and light logos on light background.

--- components/semifinals/PartnerLogo.tsx
+++ components/semifinals/PartnerLogo.tsx
@@ -41,7 +41,7 @@ export function PartnerLogo({ partner, logo }: { partner: string; logo?: string 
         alt={`${partner} logo`}
         width={56}
         height={56}
-        className="h-14 w-14 shrink-0 object-contain saturate-0 brightness-50"
+        className="h-14 w-14 shrink-0 object-contain invert brightness-125"
       />
     );
   }
